738a73ecd2560a20a5596fb1b28685f228ac4b82
skskdmsdl <skskdmsdl@gmail.com>
2023-03-18 20:11:44 +0900

    소셜 로그인 userinfo interface 생성

---
+++ src/main/java/com/chat/webSocket/config/sns/oauth/provider/OAuth2UserInfo.java
@@ -0,0 +1,8 @@
+package com.chat.webSocket.config.sns.oauth.provider;
+
+public interface OAuth2UserInfo {
+	String getProviderId();
+	String getProvider();
+	String getEmail();
+	String getName();
+}
