c6ed7144ceec2263a94a640935fd5a7a5e26d095
skskdmsdl <skskdmsdl@gmail.com>
2023-03-24 21:35:02 +0900

    gitignore 변경

--- src/main/java/com/chat/webSocket/config/sns/oauth/provider/OAuth2UserInfo.java
+++
@@ -1,8 +0,0 @@
-package com.chat.webSocket.config.sns.oauth.provider;
-
-public interface OAuth2UserInfo {
-	String getProviderId();
-	String getProvider();
-	String getEmail();
-	String getName();
-}
