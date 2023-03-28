52d93add94aecf2286e57630282c2bd34666c33c
skskdmsdl <skskdmsdl@gmail.com>
2023-03-28 22:34:24 +0900

    로그인 완료 후 호출 페이지 설정

--- src/main/java/com/chat/webSocket/member/controller/MemberController.java
+++ src/main/java/com/chat/webSocket/member/controller/MemberController.java
@@ -1,7 +1,16 @@
 package com.chat.webSocket.member.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/")
+    public Object greeting(@AuthenticationPrincipal Object user){
+        return user;
+    }
 }
